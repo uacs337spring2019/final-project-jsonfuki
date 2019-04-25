6bb3aef91a7c2fcd1c41e661cee2decaef705bf3
JasonTaro <42520129+JasonTaro@users.noreply.github.com>
2019-04-25 13:05:57 -0700

    Update zach.js

--- public/zach.js
+++ public/zach.js
@@ -49,6 +49,7 @@
 			fetch(url, mode) //fetchOptions
 				.then(checkStatus)
 				.then(function(responseText){
+				console.log(repsonseText);
 					document.getElementById("appname").style.visibility = "hidden";
 					let successDiv = document.getElementById("success");
 					successDiv.style.display = "block";
